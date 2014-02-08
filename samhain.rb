e11d1aa52db4d80063a7e42b6a91cd9c76afb6a9
Chris Neale <cin210@nyu.edu>
2014-02-07 16:34:06 -0800

    Config with w/ git ignore

--- samhain.rb
+++ samhain.rb
@@ -1,9 +1,11 @@
 require 'sinatra'
 require 'sinatra/namespace'
+require "sinatra/config_file"
 
 class Samhain < Sinatra::Base
   register Sinatra::Namespace
-  
+  register Sinatra::ConfigFile
+  config_file "./config.yml"
   get '/' do
     "All Systems Go"
   end
