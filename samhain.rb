cac5781657134cadedbc8fb942528b968a403b4d
Chris Neale <cin210@nyu.edu>
2014-02-23 19:54:11 -0800

    Parse one companies jobs at a time

--- samhain.rb
+++ samhain.rb
@@ -23,7 +23,7 @@ class Samhain < Sinatra::Base
   
   namespace "/scrape" do
     get "/test" do
-      send_to_recruiter({job: {title: "test", source_url: "test"}.to_json}.to_json)
+      send_to_recruiter({job: {title: "test", source_url: "test"}})
     end
     
     get '/angel' do
@@ -33,13 +33,16 @@ class Samhain < Sinatra::Base
       rescue Exception => ex
         puts ex
       end
-    
+      return "Failed to fetch startups" unless data
       startup_ids = data.at_css(".startup-container").attributes["data-startup_ids"].text.strip
       startup_ids.gsub! /\[(.*)\]/, '\1'
       startup_ids = startup_ids.split(",")
     
       body = ""
-      startup_ids.first(1).each do |startup_id|
+      # we want to go through each startup and get its jobs info
+      # this will ping x companies
+      number_of_companies = 1
+      startup_ids.first(number_of_companies).each do |startup_id|
         puts "scraping #{startup_id}"
         url = URI.parse("#{ANGEL_API_URL}/1/startups/#{startup_id}/jobs")
         begin 
@@ -47,23 +50,25 @@ class Samhain < Sinatra::Base
           http = Net::HTTP.new(url.host, url.port)
           http.use_ssl = true
           response = http.request(req)
-          
-          job_info = {
-            job: {
-              source_url: url,
-              source_id: startup_id,
-              timestamp: Time.now,
-              json: response.body,
+          list_of_jobs = JSON.parse(response.body)
+          puts "Loading #{list_of_jobs.count} jobs from #{startup_id}"
+          list_of_jobs.each do |job_json|
+            job_info = {
+                source_url: url,
+                source_id: startup_id,
+                timestamp: Time.now,
+                json: job_json.to_json,
             }
-          }
-          puts job_info
-          send_to_recruiter(job_info)
-          job_info
+            send_to_recruiter(job_info)
+            body += job_info.to_s if job_info
+          end
+
+          #http.finish
         rescue Exception => ex
           puts "Exeption: #{ex}"
         end
       end
-      body += job_info.to_s if job_info
+
       body
     end
   end
@@ -81,9 +86,10 @@ class Samhain < Sinatra::Base
 
     request = Net::HTTP::Post.new(uri.request_uri)
     request["Content-Type"] = "application/json"
-    request.set_form_data(data)
+    request.set_form_data(job: data.to_json)
     response = http.request(request)
     response.body
+    #http.finish
   end
   
 end
